e017ce33607d7082621a6bdb50ff07dbbb613719
ysl2 <www.songli.yu@gmail.com>
2023-11-15 16:08:47 +0800

    Change alsa master patch to my modified version.

--- components/alsa_master_vol.c
+++ components/alsa_master_vol.c
@@ -1,64 +1,43 @@
 /* Created by William Rabbermann */
+/* Refractored by Songli Yu */
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include "../util.h"
 
-#define TMP_BUF_SIZE 14
-#define VOL_BUF_SIZE 5
+#define BUFSIZE 15
+#define ITEMS 3 // volube, db, mute
 
 const char *
 alsa_master_vol(void)
 {
-	bool MASTER_IS_MUTED = true;
-	char tmp_buf[TMP_BUF_SIZE];
-	short b;
-	unsigned short i = 0;
-	
-	FILE *fp = popen("amixer get Master | tail -c13", "r");
+	char recorder[ITEMS][BUFSIZE];
+	int isrecord = 0;
+	int i; // inner pointer
+	int j = 0; // outer pointer
+
+	FILE *fp = popen("amixer -M get Master | tail -n 1", "r"); // Get the last line of command output.
 	char ch;
-	while ((ch = fgetc(fp)) != EOF && i < TMP_BUF_SIZE)
-		tmp_buf[i++] = ch;
-	tmp_buf[i] = '\0';
+
+	while ((ch = fgetc(fp)) != EOF && i < BUFSIZE && j < ITEMS) {
+		if ('[' == ch) {
+			isrecord = 1;
+			i = 0;
+			continue; // Don't record the '['
+		} else if (']' == ch) {
+			recorder[j][i] = '\0';
+			j++;
+			isrecord = 0;
+		}
+		if (isrecord) {
+			recorder[j][i++] = ch;
+		}
+	}
 	pclose(fp);
 
-	b = i - 1;
-	while (b >= 0)
-	{
-		if ('[' == tmp_buf[b])
-		{
-			if (tmp_buf[b+1] == 'o' && tmp_buf[b+2] == 'n')
-				MASTER_IS_MUTED = false;
-			b -= 3;
-			break;
-		}
-		b--;
-	}
-	
-	if (MASTER_IS_MUTED) return bprintf("MUTE");
-	else
-	{
-		char vol_buf[VOL_BUF_SIZE];
-		while (b >= 0)
-		{
-			if ('[' == tmp_buf[b])
-				break;	
-			b--;
-		}
-		
-		i = 0;
-		while (i < VOL_BUF_SIZE)
-		{
-			b++;
-			if (']' == tmp_buf[b])
-			{
-				vol_buf[i] = '\0'; 
-				break;
-			}
-			else
-				vol_buf[i++] = tmp_buf[b];
-		}
-		
-		return bprintf("%s", vol_buf);
-	}
+	// If equal, the strcmp() will return 0;
+	// So here if not equal, the if will capture.
+	if (strcmp("on", recorder[ITEMS - 1])) // If not equal.
+		return bprintf("MUTE");
+	return bprintf("%s", recorder[0]);
 }
